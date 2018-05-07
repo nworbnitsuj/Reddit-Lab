288d0655d9ad01fe55f73163f7762c5affff9898
Justin Brown <justin.v.brown@gmail.com>
2018-05-07 17:08:22 -0500

    Created Comment model in comment.js

--- back-end/models/comment.js
+++ back-end/models/comment.js
@@ -1,1 +1,12 @@
 // Add your comment model here
+let mongoose = require('mongoose');
+let Schema = Mongoose.Schema;
+
+let CommentSchema = new Schema({
+  content: String;
+  votes: Number
+});
+
+let Comment = mongoose.model('Comment', CommentSchema);
+
+module.exports = Comment;
