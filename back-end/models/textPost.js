cb87321d11489ab28b5d30a806f4994b301a6ae3
Justin Brown <justin.v.brown@gmail.com>
2018-05-07 16:31:21 -0500

    Edited textPost.js

--- back-end/models/textPost.js
+++ back-end/models/textPost.js
@@ -1,1 +1,16 @@
 // Add your textPost model here
+let mongoose = require('mongoose');
+let Schema = Mongoose.Schema;
+let Comment = require('./comment').Schema;
+
+let TextPostSchema = new Schema({
+  title: String,
+  content: String,
+  thumbnail_image_url: String,
+  votes: String,
+  comments: [Comment]
+});
+
+let TextPost = mongoose.model('TextPost', TextPostSchema);
+
+module.exports = TextPost;
