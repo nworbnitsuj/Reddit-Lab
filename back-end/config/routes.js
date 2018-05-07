28b14542ce329bb11ec02d97e46f6a790ab94917
Justin Brown <justin.v.brown@gmail.com>
2018-05-07 17:09:25 -0500

    Added TextPost routes in routes.js

--- back-end/config/routes.js
+++ back-end/config/routes.js
@@ -4,6 +4,11 @@ var textPostsController = require('../controllers/textPosts');
 var commentsController = require('../controllers/comments');
 
 //TextPost Routes
+router.get('/api/posts', textPostsController.index);
+router.post('/api/posts', textPostsController.create);
+router.get('/api/posts/:post_id', textPostsController.show);
+router.put('/api/posts/:post_id', textPostsController.update);
+router.delete('/api/posts/:post_id', textPostsController.destroy);
 
 //Comment Routes
 
